60825db15cce2527e1eb13d99e90d42e02043a34
NotRoyadma <82922092+NotRoyadma@users.noreply.github.com>
2022-12-11 13:10:12 +0530

    added somethig called lines

--- src/pages/page.js
+++ src/pages/page.js
@@ -9,7 +9,7 @@ export default async function () {
     licenceChecker()
 
     const { pages } = process.licence
-    if(!pages || !pages.default || !pages[pages.default] || !pages[pages.default].font || !pages[pages.default].textColor || !pages[pages.default].options || pages[pages.default].options.length === 0) {
+    if (!pages || !pages.default || !pages[pages.default] || !pages[pages.default].font || !pages[pages.default].textColor || !pages[pages.default].options || pages[pages.default].options.length === 0) {
         error("No page found to display!")
         process.exit(1)
     }
@@ -18,22 +18,67 @@ export default async function () {
         output: process.stdout
     })
 
+    /*
+        Define the page and the top and bottom texts
+    */
     const page = pages[pages.default]
-    if(page.scripts && page.scripts.length != 0) {
+
+    /*
+        If there is any scripts to run.
+    */
+    if (page.scripts && page.scripts.length != 0) {
         performEntryScripts(page.scripts)
     }
+
+    /*
+        The Top headline
+    */
+    if (page.top && page.top.length != 0) {
+        const top = page.top
+        top.map((v, i) => {
+            const top = v
+            if (chalk[top.textColor]) {
+                console.log(chalk[top.textColor](top.text))
+            } else {
+                console.log(chalk.cyanBright(top.text))
+            }
+        })
+    }
+
+    /*
+        The main Title
+    */
     custom(page.title, page.font, page.textColor)
-    console.log(chalk.blue("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"));
-    console.log("\n")
+
+    /*
+        The Bottom headline
+    */
+    if (page.bottom && page.bottom.length != 0) {
+        const bottom = page.bottom
+        bottom.map((v, i) => {
+            const bottom = v
+            if (chalk[bottom.textColor]) {
+                console.log(chalk[bottom.textColor](bottom.text))
+            } else {
+                console.log(chalk.cyanBright(bottom.text))
+            }
+        })
+    }
+
+    let validOptions = {
+
+    }
 
     // options
-    for(let i = 0; i < page.options.length; i++) {
+    for (let i = 0; i < page.options.length; i++) {
         const option = page.options[i]
-        if(chalk[option.textColor]) {
+        if (chalk[option.textColor]) {
             console.log(chalk[option.textColor](`${option.value}) ${option.name}`))
         } else {
             console.log(chalk.cyanBright(`${option.value}) ${option.name}`))
         }
+
+
     }
 
     console.log(chalk.blue("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"));
