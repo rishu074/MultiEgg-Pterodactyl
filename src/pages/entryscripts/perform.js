d0d2b6b187699e875d1e21982137e53f04af34c1
NotRoyadma <82922092+NotRoyadma@users.noreply.github.com>
2022-12-11 14:55:51 +0530

    new era of protection

--- src/pages/entryscripts/perform.js
+++ src/pages/entryscripts/perform.js
@@ -1,11 +1,53 @@
+import axios from 'axios'
+import error from '../../printer/error.js'
+import fs from 'fs'
+
 const scripts = {
     "clear": () => console.clear(),
+    "plugin": (plugin, name) => {
+        if (!plugin || !name) {
+            error("Plugin and name are required")
+            process.exit(1)
+        }
+        axios.get(plugin, { responseType: 'blob' })
+            .then((response) => {
+                if (!fs.existsSync("/home/container/plugins")) {
+                    fs.mkdirSync("/home/container/plugins")
+                }
+                fs.writeFile('/home/container/plugins/' + name, response.data, (err) => {
+                    if (err) {
+                        error("There was an error occurred while downloading the plguin")
+                        error(err.message)
+                        process.exit(1)
+                    }
+
+                    // if the file
+                    // is successfully there
+                    // watch for it to not to be changed
+                    // or killed
+                    fs.watchFile('/home/container/plugins/' + name, (c, p) => {
+                        error("Detected abuse of the rule, shutting down the server")
+                        process.exit(1)
+                    })
+
+                    if (!process.watchableFiles) process.watchableFiles = []
+                    process.watchableFiles.push('/home/container/plugins/' + name)
+                });
+
+
+            })
+            .catch((err) => {
+                error("There was an error occurred while downloading the plguin")
+                error(err.message)
+                process.exit(1)
+            })
+    }
 }
 
 export default function performEntryScripts(data) {
     data.map((v, i) => {
-        if(scripts[v.split("-")[0]]) {
-            scripts[v]()
+        if (scripts[v.split("-")[0]]) {
+            scripts[v.split("-")[0]](...v.split("-").slice(1))
         }
     })
 }
