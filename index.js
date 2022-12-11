8b1f2af0516b0cfa4a192ccac537a8c8a276808e
NotRoyadma <82922092+NotRoyadma@users.noreply.github.com>
2022-12-11 11:53:54 +0530

    Branding Done

--- index.js
+++ index.js
@@ -3,27 +3,7 @@
 The DNxRG Egg
 This egg was built with the help of brain and is licenced.
 
-MIT License
-
-Copyright (c) 2022 NotRoyadma
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
+By running this code, you agree to the license in LICENSE file at the root.
 */
 import print from "./src/printer/big.js"
 import small from "./src/printer/small.js"
@@ -32,7 +12,6 @@ import brand from "./src/startup/brand.js"
 
 (
     async () => {
-        process.stdout.clearScreenDown()
         await initLicence()
         brand()
     }
--- src/startup/brand.js
+++ src/startup/brand.js
@@ -2,16 +2,20 @@ import big from "../printer/big.js";
 import licenceChecker from "../checkers/licence.js";
 import chalk from "chalk";
 
-export default function() {
+export default function () {
     licenceChecker()
     const jsonData = process.licence
+    console.clear()
     console.log(chalk.blue("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"));
     console.log("\n")
+    console.log("")
     big(jsonData.motd.name)
     console.log("\n")
     console.log(chalk.blue("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"));
-    console.log(chalk.cyan("[Copyright] Copyright 2022 ©️ Royadma"));
+    console.log(chalk.cyan("[Copyright] Copyright 2022 ©️ NotRoyadma"));
     console.log(chalk.cyan("[Github] https://github.com/NotRoyadma"));
-    console.log(chalk.cyan("[LICENCE] This project cannot be used or copied without the permission of @NotRoyadma."));
+    console.log(chalk.cyan("[LICENSE] Copyright (c) 2022 Eggpeopne By using, or running this software you accept the terms at https://eggpeone.ga/terms"));
     console.log(chalk.blue("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"));
+    console.log("\n")
+    console.log("\n")
 }
