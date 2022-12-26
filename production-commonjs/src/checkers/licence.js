181aa576ae496970c36c30ef07b61ac4b0b479f5
NotRoyadma <82922092+NotRoyadma@users.noreply.github.com>
2022-12-26 17:17:57 +0530

    Update licence.js

--- production-commonjs/src/checkers/licence.js
+++ production-commonjs/src/checkers/licence.js
@@ -1,3 +1,5 @@
+const error = require('../printer/error.js')
+
 module.exports = function licenceChecker() {
     const jsonData = process.licence
     if(!jsonData || typeof jsonData != "object") {
